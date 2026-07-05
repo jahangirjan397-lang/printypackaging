e71106e58467ccdf110786104825a60a9ea6a806
M Jahangir <yor-jahangirjan397@gmail.com>
2026-07-05 15:24:02 +0500

    Level 40 optimized robots file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,17 @@
 import type { MetadataRoute } from "next";
 
+const siteUrl = "https://printypackaging.com";
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://printypackaging.com/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/thank-you"],
+      },
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
   };
 }
