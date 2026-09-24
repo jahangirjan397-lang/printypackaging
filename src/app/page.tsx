174e03f8655449674cf995ad2c9ac85a5de5fff4
jahangirjan397-lang <jahangirjan397@gmail.com>
2026-09-24 12:03:50 +0500

    Place compact payment strip after homepage quote form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,8 +52,8 @@ export default function Home() {
       <WhyChoosePrintySection />
       <OrderProcessSection />
       <IndustryBuyerSection />
-      <PaymentMethodsSection />
       <QuoteSection />
+      <PaymentMethodsSection />
     </>
   );
 }
